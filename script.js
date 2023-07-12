d9fb98bdf4f161679fc856b50132c84c2a9e96c9
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-07-12 23:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-// Your Script here.
-
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+let A = "A".charCodeAt(0); /// 65
+let Z = "Z".charCodeAt(0); // 90
 function rot13(str) {
-  let decodedArr = []; 
-  for(let i = 0 ; i < str.length ; i++) {
-	  // str = "AK#C" => "NX#P"
-	  // lookup["#"]
-	  if(lookup[str[i]] == undefined){
-			decodedArr.push(str[i])
-	  }
-	  else decodedArr.push(lookup[str[i]])
-  }
-  return decodedArr.join("") ;
+    let ans = "" ; // "" => "$N#A"
+    // str = "$A#N" => "$N#A"
+    for(let i = 0; i < str.length; i++){
+        // i = 0
+        let currentChar = str[i].charCodeAt(0);
+        if(currentChar <= Z && currentChar >= A){
+            ans += lookup[str[i]]
+        }
+        else {
+            ans += str[i] ;
+        }
+    }
+    return ans ;
 }
-
-//  "n$%op" => "a$%bc"
-
-window.rot13 = rot13;
+  
+  window.rot13 = rot13;
